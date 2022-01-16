d4857c053441ffc9db2f8fcf9386c230973522ca
Liad <liadaltiff@gmail.com>
2022-01-16 18:26:13 +0200

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
   const navigate = useNavigate();
 
   const GoToTradesPage = () => {
-    setLoggedInUser(undefined);
+    setLoggedInUser(loggedInUser);
     navigate("/trade");
   };
 
